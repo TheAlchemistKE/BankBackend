118c7917b990988740fb87e94e1b18c95de8b939
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-10-29 20:16:03 +0300

    Create main test file to setup db connection.

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,1 +1,24 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSrc = "postgresql://localhost/bank?user=njeri&password=KelynPNjeri@1998"
+)
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSrc)
+	if err != nil {
+		log.Fatal("DB Connection failed: ", err)
+	}
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
