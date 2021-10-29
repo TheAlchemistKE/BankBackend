77a21260f17767dd3d3f28cb6a4892dccd64f121
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-10-29 23:15:06 +0300

    Add tests for entries and transfers.

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,1 +1,71 @@
 package db
+
+import (
+	"context"
+	"github.com/TheAlchemistKE/BankBackend/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+
+func createRandomEntry(t *testing.T) (entry Entry) {
+	arg := CreateAccountParams{
+		Owner: util.RandomOwnerName(),
+		Balance: util.RandomAmount(),
+		Currency: util.RandomCurrency(),
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+
+	newEntry := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomAmount(),
+	}
+	entry, err1 := testQueries.CreateEntry(context.Background(), newEntry)
+	require.NoError(t, err1)
+	require.Equal(t, newEntry.AccountID, entry.AccountID)
+	require.Equal(t, newEntry.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+	return
+}
+
+func TestCreateTestEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	entry, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, entry1.AccountID, entry.AccountID)
+	require.Equal(t, entry1.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t)
+	}
+	arg := ListEntriesParams{
+		AccountID: 37,
+		Limit:     1,
+		Offset:    0,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 1)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+
+	}
+
+}
--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -1,1 +1,60 @@
 package db
+
+import (
+	"context"
+	"github.com/TheAlchemistKE/BankBackend/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createRandomTransfer(t *testing.T) (transfer Transfer){
+	arg := CreateTransferParams{
+		FromAccountID: 2,
+		ToAccountID:   3,
+		Amount:        util.RandomAmount(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	return
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createRandomTransfer(t)
+}
+
+func TestGetTransfer(t *testing.T) {
+	arg := createRandomTransfer(t)
+	transfer, err := testQueries.GetTransfer(context.Background(), arg.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+}
+
+func TestListTransfers(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t)
+	}
+	arg := ListTransfersParams{
+		FromAccountID: 2,
+		ToAccountID:   3,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+
+	}
+}
